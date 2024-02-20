717ec6edbf68e0e29d4072453877987370c4caf7
Evollt <levniko343@gmail.com>
2024-02-20 14:08:25 +0300

    create project and dockerized it

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import type { Request, Response } from 'express'
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT ?? 3000
+
+app.use(express.json())
+
+app.get('/', (request: Request, response: Response) => {
+  return response.json({
+    message: 'you\'re use me'
+  })
+})
+
+app.listen(PORT, () => {
+  console.log('Сервер был запущен на порту ' + PORT);
+})
